aa61980d3e9601ac1d26c4cc611f5c121b00cf27
Maria Naimska <mnaimska@ENRS.LOCAL>
2021-03-31 22:20:54 +0200

    menu burger

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,12 @@
 import React from "react";
-
+import {useState} from "react";
 const Navigation =()=>{
+        const [toggleMenu, setToggleMenu] = useState(true);
+    const toggle=()=>{
+        setToggleMenu(!toggleMenu);
+
+    }
+
     return (<>
     <nav>
 
@@ -18,6 +24,17 @@ const Navigation =()=>{
             <li className="navigation-li"><a href="#">Contact me</a></li>
             </div>
         </ul>
+        <div className="burger-menu container" onClick={toggle}>
+            {toggleMenu? <>
+                <div className="burger-menu-div1"></div>
+                <div className="burger-menu-div2"></div>
+                <div className="burger-menu-div3"></div></>:
+                <>
+                <div className="burgerX1"></div>
+                <div className="burgerX2 "></div>
+                <div className="burgerX3 "></div></> }
+
+        </div>
     </nav>
     </>)
 }
