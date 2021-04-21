b16a2f60168801ff8bf31339c451a5d3bc3d177e
Maria Naimska <mnaimska@ENRS.LOCAL>
2021-04-21 17:33:49 +0200

    no router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.scss';
 import React from "react";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-import * as ROUTES from "../src/components/Routes";
-import Work from "./components/Work";
+// import {BrowserRouter, Switch, Route} from "react-router-dom";
+// import * as ROUTES from "../src/components/Routes";
+// import Work from "./components/Work";
 import Home from "./components/Home";
 
 
 function App() {
   return (
-    <BrowserRouter>
-        <Switch>
-        <Route exact path={ROUTES.Home} component={Home}/>
-        <Route exact path={ROUTES.Work} component={Work}/>
-        </Switch>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //     <Switch>
+    //     <Route exact path={ROUTES.Home} component={Home}/>
+    //     <Route exact path={ROUTES.Work} component={Work}/>
+    //     </Switch>
+    // </BrowserRouter>
+      <Home/>
   );
 }
 
